38b0a2c4394fea8c083e7e909b3cfdab53fdf284
MDIDKT <152218331+MDIDKT@users.noreply.github.com>
2024-04-03 12:03:09 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MonSiteTest2</title>
+</head>
+<body>
+    <!-- Header: navbar; logo-->
+
+    <!--section1: carousel; description-->
+
+    <!--section2: services; 3 services -->
+    
+    <!--footer: contact; social media-->
+
+</body>
+</html>
