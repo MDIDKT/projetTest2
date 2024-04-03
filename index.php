fe734c416f42a037aa85ef0c5ab5431d79bd8401
MDIDKT <modidiakite@hotmail.fr>
2024-04-04 00:13:54 +0200

    page accueil

--- index.php
+++ index.php
@@ -1,18 +1,68 @@
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/style.css">
     <title>MonSiteTest2</title>
 </head>
 <body>
     <!-- Header: navbar; logo-->
-
+    <header class="headerAcceuil">
+    <img class="logo" src="assets/images/lettre-o-realiste-surface-coloree.jpg" alt="logo">
+        <nav class="headerNav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Nos services</a></li>
+                <li><a href="#">Se connecter</a></li>
+            </ul>
+        </nav>
+    </header>
     <!--section1: carousel; description-->
+    <section class="carousel">
+        <div class="carouselSlide">
+            <img src="assets/images/TaeAugust07.jpg" alt="image1">
+            <img src="assets/images/2105.q702.020.S.m005.c10.hospital room.jpg" alt="image2">
+            <img src="assets/images/lettre-o-realiste-surface-coloree.jpg" alt="image3">
+        </div>
+    </section>
 
     <!--section2: services; 3 services -->
+    <section class="services">
+        <div class="service1">
+            <img src="assets/images/lettre-o-realiste-surface-coloree.jpg" alt="service1">
+            <h3>Service1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        </div>
+        <div class="service2">
+            <img src="assets/images/lettre-o-realiste-surface-coloree.jpg" alt="service2">
+            <h3>Service2</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        </div>
+        <div class="service3">
+            <img src="assets/images/lettre-o-realiste-surface-coloree.jpg" alt="service3">
+            <h3>Service3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        </div>
     
     <!--footer: contact; social media-->
+    <footer>
+        <div class="contact">
+            <h3>Contact</h3>
+            <p>Adresse: 123 rue de Paris</p>
+            <p>Téléphone: 0123456789</p>
+            <p>Email: test@test.com</p>
+        </div>
+        <div class="socialMedia">
+            <h3>Suivez-nous</h3>
+            <a href="#"><img src="assets/images/facebook.png" alt="facebook"></a>
+            <a href="#"><img src="assets/images/instagram.png" alt="instagram"></a>
+            <a href="#"><img src="assets/images/twitter.png" alt="twitter"></a>
+        </div>
+        copyrigth 2020 
+    </footer>
 
 </body>
 </html>
